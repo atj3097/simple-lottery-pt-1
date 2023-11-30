56cbdd5902e51aec460e30c6692da5961318610d
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2023-11-30 12:55:21 -0600

    adding lottery creation logic

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -1,5 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+/*
+
+Simple lottery
+Any user can call createLottery and a lottery will be created with a ticket purchase window
+for the next 24 hours. Once the 24 hours is up, there is a 1 hour delay,
+then the lottery is over. Generating random numbers safely on Ethereum is tricky,
+but for the purpose of this, relying on a future blockhash (which the players cannot predict),
+is good enough for this project.
+After createLottery is called people can purchaseTicket for a particular lotteryId.
+The lottery must consist of a deadline for when purchasing tickets stops,
+and time afterwards when the future blockhash determines the winner.
+The winner must then claim the winnings within 256 blocks (the maximum lookback of the blockhash function),
+otherwise, everyone can get their tickets back.
+*/
 
 contract LotteryContract {
 
@@ -14,12 +28,16 @@ contract LotteryContract {
         uint256 deadline;
         address winner;
         mapping (address => Ticket) ticketOwners;
+        address [] ticketOwnersArray;
+        address owner;
     }
 
-    mapping (uint256 => Lottery) lotteryIds;
+    mapping (uint256 => Lottery) public lotteryIds;
+    uint256 public lotteryId;
 
-    function createLottery() external {
-
+    function createLottery(uint256 _prize, uint256 _deadline) external {
+        lotteryIds[lotteryId] = Lottery(lotteryId, _prize, _deadline, address(0), address(0));
+        lotteryId++;
     }
 
     function getATicket() public {
